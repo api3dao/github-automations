b60859cd17f7832502b5aa80377baa72d5df075a
Michal Kimle <kimle.michal@gmail.com>
2022-06-16 15:30:45 +0200

    Initial commit

---
+++ src/handler.ts
@@ -0,0 +1,133 @@
+import { GraphQLClient, gql } from 'graphql-request';
+import axios from 'axios';
+
+const GITHUB_GQL_API_ENDPOINT = 'https://api.github.com/graphql';
+
+const statuses = {
+  [process.env.GITHUB_PROJECT_STATUS_BACKLOG_ID!]: 'Backlog',
+  [process.env.GITHUB_PROJECT_STATUS_TODO_ID!]: 'TODO',
+  [process.env.GITHUB_PROJECT_STATUS_BLOCKED_ID!]: 'Blocked',
+  [process.env.GITHUB_PROJECT_STATUS_INPROGRESS_ID!]: 'In progress',
+  [process.env.GITHUB_PROJECT_STATUS_INREVIEW_ID!]: 'In review',
+  [process.env.GITHUB_PROJECT_STATUS_TESTING_ID!]: 'Testing',
+  [process.env.GITHUB_PROJECT_STATUS_DONE_ID!]: 'Done',
+};
+
+const graphQLClient = new GraphQLClient(GITHUB_GQL_API_ENDPOINT, {
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
+});
+
+const successResponse = () => ({
+  statusCode: 200,
+});
+
+export const run = async (event: AWSLambda.APIGatewayEvent) => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Missing event body' }),
+    };
+  }
+
+  const body = JSON.parse(event.body);
+  if (body.action !== 'edited') return successResponse();
+  if (
+    body.changes?.field_value?.field_node_id &&
+    body.changes.field_value.field_node_id !== process.env.GITHUB_PROJECT_STATUS_FIELD_EVENT_ID
+  )
+    return successResponse();
+
+  const contentNodeId = body.projects_v2_item.content_node_id;
+
+  const query = gql`
+  query {
+    node(id: "${contentNodeId}") {
+      ... on Issue {
+        title
+        url
+        number
+        repository {
+          nameWithOwner
+          url
+        }
+        projectNextItems(first: 1) {
+          nodes {
+            fieldValues(first: 20) {
+              nodes {
+                projectField {
+                  id
+                }
+                value
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
+
+  const data = await graphQLClient.request(query);
+
+  const fields = data.node.projectNextItems.nodes[0].fieldValues.nodes;
+  const statusField = fields.find(
+    (field: any) => field.projectField.id === process.env.GITHUB_PROJECT_STATUS_FIELD_GQL_ID
+  );
+
+  const issue = {
+    ...data.node,
+    status: statuses[statusField.value],
+    sender: {
+      login: body.sender.login,
+      url: body.sender.url,
+    },
+  };
+
+  await axios.post(
+    process.env.SLACK_WEBHOOK_URL!,
+    {
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Issue movement by <${issue.sender.url}|${issue.sender.login}>`,
+          },
+        },
+      ],
+      attachments: [
+        {
+          color: '#24292F',
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: `<${issue.url}|*#${issue.number} ${issue.title}*> moved to *${issue.status}*`,
+              },
+            },
+            {
+              type: 'context',
+              elements: [
+                {
+                  type: 'mrkdwn',
+                  text: `<${issue.repository.url}|${issue.repository.nameWithOwner}>`,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      timeout: 5000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  return successResponse();
+};
